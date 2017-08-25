f040081947cfbfb64e232bd24073b78796b665c1
LiXizhi <lixizhi@yeah.net>
2017-08-25 16:53:40 +0800

    pure server implementation

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-npl servermode="true" world="worlds/DesignHouse/test" ip="0.0.0.0" port="6001"
+npl servermode="true" world="worlds/DesignHouse/test" ip="0.0.0.0" port="6001" loadpackage="npl_packages/paracraft/"
